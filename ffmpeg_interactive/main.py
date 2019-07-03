1a1327c40bd174da49735d1dee95fd20bff2f619
Boomka <havoc0888@gmail.com>
2019-07-03 22:49:31 +0300

    add tests for args maker and implement it

--- ffmpeg_interactive/main.py
+++ ffmpeg_interactive/main.py
@@ -3,9 +3,40 @@ import datetime as dt
 from dateutil import parser
 
 from ffmpeg_interactive.util import add_missing_colons
+from util import add_extension, format_time, format_timedelta, get_duration
+
+
+DEFAULT_ARGS = {
+    0: 'ffmpeg',
+    40: '-c copy',
+}
 
 
 def parse_time(time_str: str) -> dt.time:
     time_str = add_missing_colons(time_str)
     dt_obj = parser.parse(time_str)
     return dt_obj.time()
+
+
+def make_args(in_file: str, out_file: str, from_time: str, to_time: str):
+    args = DEFAULT_ARGS.copy()
+
+    from_time = parse_time(from_time)
+    from_time_formatted = format_time(from_time)
+    args[10] = f'-ss {from_time_formatted}'
+
+    args[20] = f'-i "{in_file}"'
+
+    try:
+        to_time = parse_time(to_time)
+    except ValueError:
+        to_time = None
+    if to_time:
+        duration_formatted = format_timedelta(get_duration(from_time, to_time))
+        args[30] = f'-to {duration_formatted}'
+
+    args[50] = f'"{add_extension(out_file)}"'
+
+    sorted_args = tuple(v for _, v in sorted(args.items()))
+
+    return sorted_args
--- ffmpeg_interactive/util.py
+++ ffmpeg_interactive/util.py
@@ -1,3 +1,39 @@
+import datetime as dt
+
+
+VIDEO_EXTENSIONS = {'mp4', 'avi', 'wmv', 'mkv', }
+DEFAULT_EXTENSION = 'mp4'
+
+
+def add_extension(filename: str) -> str:
+    if filename.split('.')[-1] in VIDEO_EXTENSIONS:
+        return filename
+    return f'{filename}.{DEFAULT_EXTENSION}'
+
+
 def add_missing_colons(time_str: str) -> str:
     colons = time_str.count(':')
     return f'{"00:" * (2 - colons)}{time_str}'
+
+
+def format_time(time: dt.time) -> str:
+    return time.strftime('%H:%M:%S.%f')[:-3]
+
+
+def timedelta_to_time(timedelta: dt.timedelta) -> dt.time:
+    return (dt.datetime.min + timedelta).time()
+
+
+def format_timedelta(timedelta: dt.timedelta) -> str:
+    return format_time(timedelta_to_time(timedelta))
+
+
+def time_to_float(time: dt.time) -> float:
+    return time.hour * 3600 + time.minute * 60 + time.second + time.microsecond / 1e6
+
+
+def get_duration(start_time: dt.time, end_time: dt.time) -> dt.timedelta:
+    duration_seconds = time_to_float(end_time) - time_to_float(start_time)
+    if duration_seconds <= 0:
+        raise ValueError('Duration must be positive')
+    return dt.timedelta(seconds=duration_seconds)
