3e19560417cacf016a849b9100ca0edd7be0c886
Boomka <havoc0888@gmail.com>
2019-09-10 17:52:00 +0300

    Fix tests, add some new

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,28 +9,30 @@ class TestMakeArgs:
         (
             (
                 '00:04:55.123', '00:05:00.000',
-                ('ffmpeg', '-ss 00:04:55.123', '-i "input.mp4"', '-to 00:00:04.877', '-c copy', '"output.mp4"')
+                ('ffmpeg', '-hide_banner', '-ss 00:04:55.123', r'-i "C:\input.mp4"', '-to 00:00:04.877', '-c copy',
+                 '"output.mp4"')
             ),
             (
                 '00:04:55.123', '',
-                ('ffmpeg', '-ss 00:04:55.123', '-i "input.mp4"', '-c copy', '"output.mp4"')
+                ('ffmpeg', '-hide_banner', '-ss 00:04:55.123', r'-i "C:\input.mp4"', '-c copy', '"output.mp4"')
             ),
             (
                 '00:04:55.123', 'lalala',
-                ('ffmpeg', '-ss 00:04:55.123', '-i "input.mp4"', '-c copy', '"output.mp4"')
+                ('ffmpeg', '-hide_banner', '-ss 00:04:55.123', r'-i "C:\input.mp4"', '-c copy', '"output.mp4"')
             ),
             (
                 '10', '20',
-                ('ffmpeg', '-ss 00:00:10.000', '-i "input.mp4"', '-to 00:00:10.000', '-c copy', '"output.mp4"')
+                ('ffmpeg', '-hide_banner', '-ss 00:00:10.000', r'-i "C:\input.mp4"', '-to 00:00:10.000', '-c copy',
+                 '"output.mp4"')
             ),
             (
                 '10', '',
-                ('ffmpeg', '-ss 00:00:10.000', '-i "input.mp4"', '-c copy', '"output.mp4"')
+                ('ffmpeg', '-hide_banner', '-ss 00:00:10.000', r'-i "C:\input.mp4"', '-c copy', '"output.mp4"')
             ),
         )
     )
     def test_parametrized_times__returns_args(self, from_str, to_str, result_cmd):
-        result = make_ffmpeg_args('input.mp4', 'output.mp4', from_time=from_str, to_time=to_str)
+        result = make_ffmpeg_args(r'C:\input.mp4', 'output.mp4', from_time=from_str, to_time=to_str)
         assert result == result_cmd
 
     @pytest.mark.parametrize(
@@ -42,4 +44,8 @@ class TestMakeArgs:
     )
     def test_parametrized_times__raises_value_error(self, from_str, to_str):
         with pytest.raises(ValueError):
-            make_ffmpeg_args('input.mp4', 'output.mp4', from_time=from_str, to_time=to_str)
+            make_ffmpeg_args(r'C:\input.mp4', 'output.mp4', from_time=from_str, to_time=to_str)
+
+    def test_relative_infile_path__raises_value_error(self):
+        with pytest.raises(ValueError):
+            make_ffmpeg_args('input.mp4', 'output.mp4', from_time='00:04:55.123', to_time='00:05:00.000')
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,10 @@
+import datetime
+
+import freezegun
 import pytest
 
 from ffmpeg_interactive.util import add_missing_colons
+from util import parse_time
 
 
 class TestAddMissingColons:
@@ -15,3 +19,22 @@ class TestAddMissingColons:
     )
     def test_parametrized(self, time_str, expected):
         assert add_missing_colons(time_str) == expected
+
+
+class TestParseTime:
+    @freezegun.freeze_time(datetime.datetime(2000, 1, 1, 0, 0, 0))
+    @pytest.mark.parametrize(
+        'time_str, expected',
+        (
+            ('12:34', datetime.time(0, 12, 34)),
+            ('12:34:56', datetime.time(12, 34, 56)),
+            ('12 34', datetime.time(0, 12, 34)),
+            ('12Ж34', datetime.time(0, 12, 34)),
+            ('12ж34', datetime.time(0, 12, 34)),
+            ('12-34', datetime.time(0, 12, 34)),
+        )
+    )
+    def test_parametrized(self, time_str, expected):
+        result = parse_time(time_str)
+        print(result)
+        assert result == expected
